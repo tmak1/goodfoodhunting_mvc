03d13462b532c844c92082369ba16956336a18dd
Daniel Tsui <tsui.daniel@gmail.com>
2020-01-28 14:39:12 +1100

    add seed and user model

--- controllers/dishes_controller.rb
+++ controllers/dishes_controller.rb
@@ -1,9 +1,17 @@
 
+
 post '/dishes' do
-  create_dish(params[:name], params[:image_url], current_user['id'])
+  create_dish(
+    params[:name], 
+    params[:image_url], 
+    current_user['id']
+  )
   redirect "/"
 end
 
+get '/dishes/new' do
+  erb :"/dishes/new"
+end
 
 get '/dishes/:id' do
   @dish = find_one_dish_by_id(params[:id])
---
+++ models/user.rb
@@ -0,0 +1,28 @@
+# read
+def all_users
+  sql = "SELECT * FROM users;"
+  # implicit return
+  run_sql(sql)
+end
+
+def find_one_user_by_email(email)
+  sql = "SELECT * FROM dishes WHERE email = $1;"
+  run_sql(sql, [email])[0]
+end
+
+# create
+require 'bcrypt'
+def create_user(email, password)
+  password_digest = BCrypt::Password.create(password)
+
+  sql = <<~SQL
+    INSERT INTO users 
+    (email, password_digest)
+    VALUES
+    ($1, $2);
+  SQL
+  run_sql(sql, [email, password_digest])
+end
+
+# delete - todo
+# update - todo
